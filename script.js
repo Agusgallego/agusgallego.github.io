8687a44e5001dee1df90ab0f753b8a00b9eb40ff
AgusGallego <166161311+Agusgallego@users.noreply.github.com>
2024-08-28 13:42:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,45 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+const dynamicText = document.querySelector("h1 span");
+const words = ["Agus Gallego", "SOC N1 ", "PenetrationTester", "EthicalHacker"];
+let wordIndex = 0;
+let charIndex = 1;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    
+    if(!isDeleting && charIndex < currentWord.length){
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    }else if(isDeleting && charIndex > 0){
+        charIndex--;
+        setTimeout(typeEffect,100);
+    }else{
+        isDeleting = !isDeleting;
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+typeEffect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
